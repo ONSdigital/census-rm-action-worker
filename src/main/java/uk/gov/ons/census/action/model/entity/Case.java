37f50c251c2cad893976a7f7fb0069f1c37ab4fb
liamtoozer <liam.toozer@gmail.com>
2020-02-24 14:55:32 +0000

    Update model

--- src/main/java/uk/gov/ons/census/action/model/entity/Case.java
+++ src/main/java/uk/gov/ons/census/action/model/entity/Case.java
@@ -93,6 +93,6 @@ public class Case {
   @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
   private boolean undeliveredAsAddressed;
 
-  @Column(columnDefinition = "BOOLEAN DEFAULT false")
+  @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
   private boolean handDelivery;
 }
