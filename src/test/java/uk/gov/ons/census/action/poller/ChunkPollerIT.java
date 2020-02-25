0d70d03758ef51532df3c4a8be8313b55c786e48
liamtoozer <liam.toozer@gmail.com>
2020-02-25 10:25:46 +0000

    RMC-317: Add handDelivery on outbound messages (#10)

--- src/test/java/uk/gov/ons/census/action/poller/ChunkPollerIT.java
+++ src/test/java/uk/gov/ons/census/action/poller/ChunkPollerIT.java
@@ -288,6 +288,7 @@ public class ChunkPollerIT {
 
     assertThat(actualFieldworkFollowup.getCeActualResponses())
         .isEqualTo(randomCase.getCeActualResponses());
+    assertThat(actualFieldworkFollowup.getHandDelivery()).isEqualTo(randomCase.isHandDelivery());
   }
 
   private UacQidDTO stubCreateWelshUacQid() throws JsonProcessingException {
