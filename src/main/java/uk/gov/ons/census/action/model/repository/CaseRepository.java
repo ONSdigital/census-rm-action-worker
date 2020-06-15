ce6a76c63342e5939987e42028a5f0cac892a017
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-06-15 17:27:23 +0100

    Update CaseRepository.java

--- src/main/java/uk/gov/ons/census/action/model/repository/CaseRepository.java
+++ src/main/java/uk/gov/ons/census/action/model/repository/CaseRepository.java
@@ -1,7 +1,6 @@
 package uk.gov.ons.census.action.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import uk.gov.ons.census.action.model.entity.Case;
 
-public interface CaseRepository extends JpaRepository<Case, Long>, JpaSpecificationExecutor<Case> {}
+public interface CaseRepository extends JpaRepository<Case, Long> {}
