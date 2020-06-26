6d96d60a2de3897a1d89c47e707e2bf5514997ad
LeoKHoward <leo-howard@hotmail.co.uk>
2020-06-26 10:27:40 +0100

    Reformatting

--- src/main/java/uk/gov/ons/census/action/builders/UacQidLinkBuilder.java
+++ src/main/java/uk/gov/ons/census/action/builders/UacQidLinkBuilder.java
@@ -41,9 +41,12 @@ public class UacQidLinkBuilder {
           ActionType.SPG_IC12,
           ActionType.SPG_IC13,
           ActionType.SPG_IC14
-          // This list is only for INITIAL CONTACT letters/questionnaires that are not part of expected capacity for
-          // the case. You should only add to it if you are certain that you are adding some new initial contact
-          // printed materials which is unlikely. For security reasons, initial contact UACs should never be mailed
+          // This list is only for INITIAL CONTACT letters/questionnaires that are not part of
+          // expected capacity for
+          // the case. You should only add to it if you are certain that you are adding some new
+          // initial contact
+          // printed materials which is unlikely. For security reasons, initial contact UACs should
+          // never be mailed
           // out a second time, because some respondents will have partially completed their EQs.
           );
 
