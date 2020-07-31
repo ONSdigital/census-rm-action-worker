3ab86cab64463f8330a0ae9bd72f24db02302cbf
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2020-07-31 14:17:03 +0100

    changed pack code 4A to 4

--- src/main/java/uk/gov/ons/census/action/builders/PrintFileDtoBuilder.java
+++ src/main/java/uk/gov/ons/census/action/builders/PrintFileDtoBuilder.java
@@ -20,7 +20,7 @@ public class PrintFileDtoBuilder {
           ActionType.P_RL_1IRL2B,
           ActionType.P_RL_1RL1B,
           ActionType.P_RL_1RL2BB,
-          ActionType.P_RL_1RL4A
+          ActionType.P_RL_1RL4
           // Adding to this list will result in no UAC being sent on the printed
           // letter/questionnaire. Please be certain of what you're doing before adding to it.
           );
