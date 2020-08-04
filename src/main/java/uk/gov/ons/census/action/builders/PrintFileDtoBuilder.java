a3269711c1559c89fd93c67d694302c34ea6c842
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2020-08-04 09:17:13 +0100

    removing action types added in error

--- src/main/java/uk/gov/ons/census/action/builders/PrintFileDtoBuilder.java
+++ src/main/java/uk/gov/ons/census/action/builders/PrintFileDtoBuilder.java
@@ -14,14 +14,10 @@ public class PrintFileDtoBuilder {
       Set.of(
           ActionType.P_RL_1RL1A,
           ActionType.P_RL_1RL2BA,
-          ActionType.P_RL_2RL1,
-          ActionType.P_RL_2RL2B,
           ActionType.P_RL_2RL1A,
           ActionType.P_RL_2RL2BA,
           ActionType.P_RL_1IRL1,
           ActionType.P_RL_1IRL2B,
-          ActionType.P_RL_1RL1B,
-          ActionType.P_RL_1RL2BB,
           ActionType.P_RL_1RL4
           // Adding to this list will result in no UAC being sent on the printed
           // letter/questionnaire. Please be certain of what you're doing before adding to it.
